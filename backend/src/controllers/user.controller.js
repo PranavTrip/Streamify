ccd5f3dd2d14c7bdd09bb3d1e0035f3015b2eb18
Pranav Tripathi <pranavtri00@gmail.com>
2025-07-30 15:27:51 +0530

    Add User routes and controllers

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,138 @@
+import FriendRequest from "../models/FriendRequest.js";
+import User from "../models/user.js";
+
+export async function getRecommendedUsers(req, res) {
+    try {
+        const currentUserId = req.user.id;
+        const currentUser = req.user;
+
+        const recommendedUsers = await User.find({
+            $and: [
+                { _id: { $ne: currentUserId } }, //exclude current user
+                { _id: { $nin: currentUser.friends } }, //exclude friends of current user
+                { isOnboarded: true }
+            ]
+        })
+        res.status(200).json(recommendedUsers)
+    } catch (error) {
+        console.error('Error fetching recommended users:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
+
+export async function getMyFriends(req, res) {
+    try {
+        const user = await User.findById(req.user.id).select('friends')
+            .poplate('friends', 'fullName profilePic nativeLanguage learningLanguage');
+
+        res.status(200).json(user.friends);
+    } catch (error) {
+        console.error('Error fetching friends:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
+
+export async function sendFriendRequest(req, res) {
+    try {
+        const myId = req.user.id
+        const { id: recipientId } = req.params;
+
+        if (myId === recipientId) {
+            return res.status(400).json({ success: false, message: 'You cannot send a friend request to yourself.' });
+        }
+
+        const recipient = await User.findById(recipientId);
+        if (!recipient) {
+            return res.status(404).json({ success: false, message: 'Recipient not found.' });
+        }
+
+        if (recipient.friends.includes(myId)) {
+            return res.status(400).json({ success: false, message: 'You are already friends with this user.' });
+        }
+
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },
+                { sender: recipientId, recipient: myId }
+            ]
+        })
+        if (existingRequest) {
+            return res.status(400).json({ success: false, message: 'A friend request already exists between you and this user.' });
+        }
+
+        const friendRequest = await FriendRequest.create({
+            sender: myId,
+            recipient: recipientId
+        })
+        res.status(201).json(friendRequest)
+    } catch (error) {
+        console.error('Error sending friend request:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+
+    }
+}
+
+export async function acceptFriendRequest(req, res) {
+    try {
+        const { id: requestId } = req.params;
+
+        const friendRequest = await FriendRequest.findById(requestId)
+        if (!friendRequest) {
+            return res.status(404).json({ success: false, message: 'Friend request not found.' });
+        }
+
+        if (friendRequest.recipient.toString() !== req.user.id) {
+            return res.status(403).json({ success: false, message: 'You are not authorized to accept this friend request.' });
+        }
+
+        friendRequest.status = 'accepted';
+        await friendRequest.save();
+
+        // Add each user to the other's friends list
+        await User.findByIdAndUpdate(friendRequest.sender, {
+            $addToSet: { friends: friendRequest.recipient }
+        })
+        await User.findByIdAndUpdate(friendRequest.recipient, {
+            $addToSet: { friends: friendRequest.sender }
+        })
+
+        res.status(200).json({ success: true, message: 'Friend request accepted successfully.' })
+
+    } catch (error) {
+        console.error('Error accepting friend request:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
+
+
+export async function getFriendRequests(req, res) {
+    try {
+        const incomingRequest = await FriendRequest.find({ recipient: req.user.id, status: 'pending' })
+            .populate('sender', 'fullName profilePic nativeLanguage learningLanguage');
+
+        const accpetedRequests = await FriendRequest.find({ sender: req.user.id, status: 'accepted' })
+            .populate('recipient', 'fullName profilePic');
+
+        res.status(200).json({
+            incomingRequests,
+            acceptedRequests
+        });
+
+    } catch (error) {
+        console.error('Error fetching friend requests:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+
+    }
+}
+
+export async function getOutgoingFriendRequests(req, res) {
+    try {
+        const outgoingRequests = await FriendRequest.find({ sender: req.user.id, status: 'pending' })
+        .populate("recipient", "fullName profilePic nativeLanguage learningLanguage");
+        res.status(200).json(outgoingRequests);
+    } catch (error) {
+        console.error('Error fetching outgoing friend requests:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+
+    }
+}
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { protectedRoute } from '../middleware/auth.middleware.js'
+import { acceptFriendRequest, getFriendRequests, getMyFriends, getOutgoingFriendRequests, getRecommendedUsers, sendFriendRequest } from '../controllers/user.controller.js'
+
+const router = express.Router()
+
+// Apply auth middleware to all routes in this file
+router.use(protectedRoute)
+router.get('/', getRecommendedUsers)
+router.get('/friends', getMyFriends)
+
+router.post('/friend-request/:id', sendFriendRequest)
+router.put('/friend-request/:id/accept', acceptFriendRequest)
+
+router.get('/friend-requests', getFriendRequests)
+router.get('/outgoing-friend-requests', getOutgoingFriendRequests)
+
+
+export default router
