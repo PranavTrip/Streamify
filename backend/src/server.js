d6257c02f6aa75068136c4049aae2ee0124a36bc
Pranav Tripathi <pranavtri00@gmail.com>
2025-07-31 17:04:51 +0530

    Fixes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,22 +6,35 @@ import chatRoutes from './routes/chat.route.js'
 import { connectToDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from "path";
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
-const FRONTEND_URL = process.env.FRONTEND_URL
 
 app.use(cors({
-    origin: FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true
 }))
+
+const __dirname = path.resolve();
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/chat', chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 
 app.listen(PORT, () => {
